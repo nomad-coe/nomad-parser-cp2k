285d5b9e10588cfa7e7f804460381ae09e298725
Himanen, Lauri (himanel1) <lauri.himanen@aalto.fi>
2016-02-04 09:52:13 +0200

    Comment.

--- parser/parser-cp2k/cp2kparser/scalainterface.py
+++ parser/parser-cp2k/cp2kparser/scalainterface.py
@@ -7,7 +7,7 @@ from cp2kparser.parsing.versions.cp2k262.outputparser import CP2KOutputParser
 from nomadcore.local_meta_info import loadJsonFile, InfoKindEl
 from nomadcore.simple_parser import mainFunction
 
-# This is what gets run when the scala layer calls for this parser. Currently
+# This is what should be called by the scala layer to run the parser. Currently
 # only the outputparser is used because the scala layer doesn't support
 # auxiliary files. Also the version identification is skipped and the structure
 # used in CP2K 2.6.2 is assumed.
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     outputparser = CP2KOutputParser(None, None)
 
     # Setup the metainfos
-    metaInfoPath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../../nomad-meta-info/meta_info/nomad_meta_info/{}".format(cp2kparser.get_metainfo_filename())))
+    metaInfoPath = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../../../../nomad-meta-info/meta_info/nomad_meta_info/{}".format(cp2kparser.get_metainfo_filename())))
     metaInfoEnv, warnings = loadJsonFile(filePath=metaInfoPath, dependencyLoader=None, extraArgsHandling=InfoKindEl.ADD_EXTRA_ARGS, uri=None)
 
     # Parser info
