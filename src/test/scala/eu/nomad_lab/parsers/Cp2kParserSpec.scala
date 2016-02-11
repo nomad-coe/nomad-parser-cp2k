251d769790076de7ad5c478fb4eaa01e1066293b
ankit kariryaa <kariryaa@fhi-berlin.mpg.de>
2016-02-11 10:03:17 +0100

    Add test

---
+++ src/test/scala/eu/nomad_lab/parsers/Cp2kParserSpec.scala
@@ -0,0 +1,17 @@
+package eu.nomad_lab.parsers
+
+import org.specs2.mutable.Specification
+
+
+
+object Cp2kParserSpec extends Specification {
+  "Cp2kParserTest" >> {
+    "test with json-events" >>  {
+      ParserRun.parse(Cp2kParser,"parsers/cp2k/test/examples/Si_bulk8/Si_bulk8.out","json-events") must_== ParseResult.ParseSuccess
+    }
+  }
+
+  "test with json" >>  {
+    ParserRun.parse(Cp2kParser,"parsers/cp2k/test/examples/Si_bulk8/Si_bulk8.out","json") must_== ParseResult.ParseSuccess
+  }
+}
