e3c283e9e2f9df24fff865f7f6165267005dd874
pupu <pupu@pupu-Inspiron>
2016-05-03 15:29:05 +0300

    setup.py installs subpackages and needed data files.

--- parser/parser-cp2k/setup.py
+++ parser/parser-cp2k/setup.py
@@ -2,7 +2,7 @@
 This is a setup script for installing the parser locally on python path with
 all the required dependencies. Used mainly for local testing.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 #===============================================================================
@@ -13,13 +13,13 @@ def main():
         version="0.1",
         include_package_data=True,
         package_data={
-            '': ['*.pickle'],
+            'cp2kparser.versions.cp2k262': ['input_data/*.json', 'input_data/*.pickle'],
         },
         description="NoMaD parser implementation for CP2K",
         author="Lauri Himanen",
         author_email="lauri.himanen@gmail.com",
         license="GPL3",
-        packages=["cp2kparser"],
+        packages=find_packages(),
         install_requires=[
             'pint',
             'numpy',
