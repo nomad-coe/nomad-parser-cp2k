95743e230b16a646df3019dfbaa3f7d4d68d3fd4
Himanen, Lauri (himanel1) <lauri.himanen@aalto.fi>
2016-05-26 13:04:15 +0300

    Fixed the test file path.

--- src/test/scala/eu/nomad_lab/parsers/Cp2kParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/Cp2kParserSpec.scala
@@ -5,11 +5,11 @@ import org.specs2.mutable.Specification
 object Cp2kParserSpec extends Specification {
   "Cp2kParserTest" >> {
     "test with json-events" >> {
-      ParserRun.parse(Cp2kParser, "parsers/cp2k/test/examples/Si_bulk8/Si_bulk8.out", "json-events") must_== ParseResult.ParseSuccess
+      ParserRun.parse(Cp2kParser, "parsers/cp2k/test/examples/energy_force/si_bulk8.out", "json-events") must_== ParseResult.ParseSuccess
     }
   }
 
   "test with json" >> {
-    ParserRun.parse(Cp2kParser, "parsers/cp2k/test/examples/Si_bulk8/Si_bulk8.out", "json") must_== ParseResult.ParseSuccess
+    ParserRun.parse(Cp2kParser, "parsers/cp2k/test/examples/energy_force/si_bulk8.out", "json") must_== ParseResult.ParseSuccess
   }
 }
