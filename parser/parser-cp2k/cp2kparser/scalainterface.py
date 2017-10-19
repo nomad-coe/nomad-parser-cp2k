246a2558b80a59165ba521e0d29f8d5f51a05624
Lauri Himanen <lauri.himanen@aalto.fi>
2017-10-19 15:01:03 +0300

    Restored the accidentally removed scalainterface.

---
+++ parser/parser-cp2k/cp2kparser/scalainterface.py
@@ -0,0 +1,17 @@
+"""
+This is the access point to the parser for the scala layer in the
+NOMAD project.
+"""
+from __future__ import absolute_import
+import sys
+import setup_paths
+from nomadcore.parser_backend import JsonParseEventsWriterBackend
+from cp2kparser import CP2KParser
+
+
+if __name__ == "__main__":
+
+    # Initialise the parser with the main filename and a JSON backend
+    main_file = sys.argv[1]
+    parser = CP2KParser(backend=JsonParseEventsWriterBackend)
+    parser.parse(main_file)
