f1825fbe9a7539adcb7196fb85a1cb1ed6a237b6
Lauri Himanen <lauri.himanen@aalto.fi>
2017-10-19 14:27:32 +0300

    Simplified setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
-"""
-This is a setup script for installing the parser locally on python path with
-all the required dependencies. Used mainly for local testing.
-"""
 from setuptools import setup, find_packages
 
 
-#===============================================================================
 def main():
-    # Start package setup
     setup(
         name="cp2kparser",
         version="0.1",
@@ -21,14 +15,9 @@ def main():
         package_dir={'': 'parser/parser-cp2k'},
         packages=find_packages(),
         install_requires=[
-            'future',
-            'pint',
-            'numpy',
-            'mdtraj',
-            'ase'
+            'nomadcore',
         ],
     )
 
-# Run main function by default
 if __name__ == "__main__":
     main()
